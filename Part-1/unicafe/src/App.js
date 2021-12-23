f7cc1668f88a1b9e99dc5201ae5196cf3e7f047c
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-12-23 14:40:58 +0530

    edits

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+import React from 'react'
+
+const App = ()=>{
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  const Header = ()=>{
+
+    return (
+      <div>
+          <h1> course= {course}</h1>
+      </div>
+    )
+
+  }
+
+  const Part = ()=>{
+
+  }
+
+  const Content = ()=>{
+
+    return (
+      <div>
+        <Part part={part1} exercise={exercises1} />
+
+        <Part part={part2} exercise={exercises2}/>
+
+        <Part part={part3} exercise={exercises3}/>
+
+
+      </div>
+    )
+
+  }
+
+  const Total = ()=>{
+
+    return (
+      <div>
+          <p>
+            Number of exercises {exercises1 + exercises2 + exercises3}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    )
+
+  }
+
+
+  return (
+    <div>
+      <p> Hi</p>
+        <Header />
+
+        <Content />
+
+        <Total />
+
     </div>
-  );
+  )
+
 }
 
 export default App;
