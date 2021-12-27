1410bc908b8a7273a2fe7e68dd3a5f22e12cda3b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-12-27 12:44:52 +0530

    component refactoring

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -1,21 +1,11 @@
 import React, {useState} from 'react';
 
-const Display = (props) => {
-  return (
-    <div>
-        {props.counter}
-    </div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
+  
 
-const Button = (props) => {
 
-  return (
-    <button onClick={props.onClick}>
-        {props.text}
-    </button>
-  )
-}
+const Button = ({onClick, text}) => (<button onClick={onClick}> {text} </button>)
+  
 
 
 
