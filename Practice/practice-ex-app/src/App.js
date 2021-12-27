eabff20e2781980c3eb9ae9a28d44883f77e5e7b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2021-12-27 20:03:35 +0530

    complex state 1

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -1,36 +1,31 @@
 import React, {useState} from 'react';
 
-const Display = ({counter}) => <div>{counter}</div>
-  
 
-
-const Button = ({onClick, text}) => (<button onClick={onClick}> {text} </button>)
   
 
 
 
 const App= () => {
 
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
   
-  const increaseByOne = () => setCounter(counter+1)
-  const decreaseByOne =() => setCounter(counter -1)
-  const setToZero = () => setCounter(0)
+  
  
 
   return (
     <div>
-      <Display counter={counter} />
-        
-        
+      {left}
+      <button onClick= {()=> setLeft(left+1)}>
+        left
+      </button>
       
-      <Button onClick={increaseByOne} text='Plus'/>
+      <button onClick={ () => setRight(right+1)}>
+        right
+      </button>
 
-      <Button onClick={decreaseByOne} text='Minus'/>
-        
-     
-      <Button onClick={setToZero} text='Zero' />
+      {right}
         
       
     </div>
