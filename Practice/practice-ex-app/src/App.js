5e27d08cff2a9d65899b0022b40b1e992c7975aa
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-01-02 19:21:12 +0530

    edits

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -4,16 +4,16 @@ import React, {useState} from 'react';
 const App = () => {
   const [value, setValue] = useState(10)
   
-  const setToValue = (newValue) => () => {
+  const setToValue = (newValue)=> {
     setValue(newValue)
   }
   
   return (
     <div>
       {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
+      <button onClick={()=>setToValue(1000)}>thousand</button>
+      <button onClick={()=>setToValue(0)}>reset</button>
+      <button onClick={()=>setToValue(value + 1)}>increment</button>
     </div>
   )
 }
