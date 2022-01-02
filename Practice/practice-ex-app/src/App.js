c47944b2ac25c959a7f2aa5d031e3f87a8f9c58c
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-01-02 13:19:35 +0530

    conditional rendering

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -1,11 +1,38 @@
 import React, {useState} from 'react';
 
+const History = (props) => {
+  if(props.allClicks.length === 0){
+
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+
+  }
+
+  return (
+    
+    <div>
+      button press history: {props.allClicks.join(' ')}
+
+    </div>
+  )
+}
+
 const App= () => {
 
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
 
   const [allClicks, setAll] = useState([])
+
+  const Button = ({handleClick,text}) => {
+
+    return(
+      <button onClick={handleClick}>{text}</button>
+    )
+  }
 
 
   const handleLeftClicks = () => {  
@@ -28,17 +55,17 @@ const App= () => {
   return (
     <div>
       {left}
-      <button onClick= {handleLeftClicks}>
-        left
-      </button>
+      <Button onClick= {handleLeftClicks} text="left" />
+       
+      
 
-      <button onClick={handleRightClicks}>
-        right
-      </button>
+      <Button onClick={handleRightClicks} text="right" />
+        
+      
 
       {right}
 
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
         
       
     </div>
