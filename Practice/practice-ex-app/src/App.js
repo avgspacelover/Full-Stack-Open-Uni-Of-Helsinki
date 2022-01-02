6799d2bf4ae55dd9c30ebaad5da249db11e41a9b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-01-02 19:13:38 +0530

    eventhandling revisited

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -1,82 +1,23 @@
 import React, {useState} from 'react';
 
-const History = (props) => {
-  
-  if(props.allClicks.length === 0){
 
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
 
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) =>() => {
+    console.log('hello', who)
+    setValue(0)
   }
-
-  return (
     
-    <div>
-      button press history: {props.allClicks.join(' ')}
-
-    </div>
-  )
-}
-const Button = ({handleClick,text}) => (
   
-  
-  <button onClick={handleClick}>
-    {text}
-  </button>
-  
-  
-)
-
-const App= () => {
-
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-
-  const [allClicks, setAll] = useState([])
-
-  
-
-
-  const handleLeftClicks = () => {  
-    
-    
-    setAll(allClicks.concat('L')) 
-    setLeft(left +1)
-    
-  }
-
-  const handleRightClicks = () => { 
-     
-    
-    setAll(allClicks.concat('R'))
-    setRight(right +1)
-    
-  }
-
-  
-  
-  
- 
 
   return (
     <div>
-      {left}
-      <Button handleClick= {handleLeftClicks} text="left" />
-       
-      
-
-      <Button handleClick={handleRightClicks} text="right" />
-        
-      
-
-      {right}
-
-      <History allClicks={allClicks} />
-        
-      
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('function')}>button</button>
     </div>
   )
 }
