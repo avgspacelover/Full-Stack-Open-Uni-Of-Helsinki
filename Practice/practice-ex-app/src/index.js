0e10cf436381b371790908a40e32e1dc031595eb
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-02-02 15:51:04 +0530

    refactored response call

--- Practice/practice-ex-app/src/index.js
+++ Practice/practice-ex-app/src/index.js
@@ -2,11 +2,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-axios.get('http://localhost:3001/notes').then(response => {
-  const notes = response.data
-  console.log(notes)
-})
-
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
 
 const notes = [
   {
