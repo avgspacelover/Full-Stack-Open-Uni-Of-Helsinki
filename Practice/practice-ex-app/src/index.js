ba58f8dd2d933f477149680bdeffd12f04dbc2b0
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-02-02 15:48:44 +0530

    adding a response to promise

--- Practice/practice-ex-app/src/index.js
+++ Practice/practice-ex-app/src/index.js
@@ -5,6 +5,10 @@ import axios from 'axios'
 const promise = axios.get('http://localhost:3001/notes')
 console.log(promise)
 
+promise.then(response => {
+  console.log(response)
+})
+
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
 
