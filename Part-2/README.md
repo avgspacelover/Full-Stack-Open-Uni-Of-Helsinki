957bb21f207dc8a191639c8d5ccf21a413c8803f
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-02-02 23:51:41 +0530

    edits

--- Part-2/README.md
+++ Part-2/README.md
@@ -1,4 +1,4 @@
 - Task List 
 
-    - 2.12 
+    - 2.12 create a react app to display data of a country when typed the specific name  and show list of countries matching if less than 10
     -  
--- Part-2/countrydb/src/App.js
+++ Part-2/countrydb/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  //const [country, Setcountry]= useState([])
+
+  useEffect(() => {
+    axios
+      .get('')
+      .then(response => {
+        console.log(response)
+      })
+  },[])
+
+
+
+//<input value={} onChange={} />
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <h3>Search Country Database</h3>
+
+        
+      </section>
     </div>
+    
   );
 }
 
