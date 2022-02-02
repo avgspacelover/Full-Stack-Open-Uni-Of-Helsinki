d4bd26188d656b0ef8f3332707ef35438acb88bc
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-02-02 15:49:28 +0530

    removed promise variable

--- Practice/practice-ex-app/src/index.js
+++ Practice/practice-ex-app/src/index.js
@@ -2,15 +2,11 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
-
-promise.then(response => {
-  console.log(response)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  console.log(notes)
 })
 
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
 
 const notes = [
   {
