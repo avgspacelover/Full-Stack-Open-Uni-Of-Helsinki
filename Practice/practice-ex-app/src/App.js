134d0921341d4c0f72aee38f30ad5ba862531967
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-02-24 02:08:22 +0530

    edits

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -37,8 +37,14 @@ useEffect(hook, [])
 */
 
  const toggleImportanceOf= (id)=> {
-   console.log(`importance of ${id} needs to be toggled`)
- }
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
 
 
 
