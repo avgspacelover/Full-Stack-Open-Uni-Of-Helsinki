9827c40f7587e617b4e867a45333d977eb4071e2
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-02 12:25:26 +0530

    2-e-2

--- Practice/practice-ex-app/src/App.js
+++ Practice/practice-ex-app/src/App.js
@@ -14,6 +14,20 @@ const Notification = ({ message }) => {
   )
 }
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  )
+}
+
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -120,6 +134,8 @@ const App = (props) => {
             />
           <button type="submit">save</button>
       </form>
+
+      <Footer />
     </div>
   )
 }
--- Practice/practice-ex-app/src/services/notes.js
+++ Practice/practice-ex-app/src/services/notes.js
@@ -8,6 +8,19 @@ const getAll = () => {
 
 }
 
+// checking for toggleImportance error message working
+
+// const getAll = () => {
+//     const request = axios.get(baseurl)
+//     const nonExisting = {
+//       id: 10000,
+//       content: 'This note is not saved to server',
+//       date: '2019-05-30T17:30:31.098Z',
+//       important: true,
+//     }
+//     return request.then(response => response.data.concat(nonExisting))
+//   }
+
 const create = (newObject) => {
     const request= axios.post(baseurl,newObject)
     return request.then(response => response.data)
