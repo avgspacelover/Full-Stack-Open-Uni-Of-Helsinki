2b7ec1de3b12da538612c5f9e85cb8cdccceb35d
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-09 15:19:28 +0530

    edits

--- Part-3/phonebook-backend/index.js
+++ Part-3/phonebook-backend/index.js
@@ -6,7 +6,17 @@
 
     app.use(express.json())
 
-    //app.use()
+    morgan.token('data', function(req, res) {
+
+        
+        return JSON.stringify(req.body);
+    })
+
+    app.use(
+        morgan(":method :url :status :res[content-length] - :response-time ms :data")
+    )
+
+    
     
     let persons = [
         { 
@@ -54,15 +64,7 @@
     })
 
 
-    morgan.token('data', function(req, res) {
-
-        
-        return JSON.stringify(req.body);
-    })
-
-    app.use(
-        morgan(":method :url :status :res[content-length] - :response-time ms :data")
-      )
+    
 
 
 
