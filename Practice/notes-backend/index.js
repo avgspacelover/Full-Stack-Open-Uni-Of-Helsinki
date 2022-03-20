d7e2ae4bf7412ac97caf7b333f4bc7d43cfb79a5
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-03-20 14:45:48 +0530

    installed dotenv+edits

--- Practice/notes-backend/index.js
+++ Practice/notes-backend/index.js
@@ -90,45 +90,38 @@ const Note = mongoose.model('Note', noteSchema)
     })
  })
 
- app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  
-  if (note) {
+ //  const generateId = () => {
+
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id)) 
+//     : 0
+
+//   return maxId + 1
+
+//  }
+
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
- const generateId = () => {
-
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
-    : 0
-
-  return maxId + 1
-
- }
+  })
+ })
 
 app.post('/api/notes', (request,response) => {
   const body = request.body
-  
-  if(!body.content){
-    return response.status(400).json({
-      error: "content missing"
-    })
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note ={
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 
