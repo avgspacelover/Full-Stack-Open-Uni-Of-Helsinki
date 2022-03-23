ea079d1962c5f723586fe20a07de33b2d961113b
John Cambefort <57269937+johncambefort@users.noreply.github.com>
2022-03-23 10:41:16 -0400

    Move globals to be under the App component

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,37 +1,6 @@
 
 import React, {useState} from 'react'
 
-const course = {
-  name:'Half Stack application development',
-  parts: [
-  {
-    name:'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name:'Using props to pass data',
-    exercises:7
-  },
-  {
-    name:'State of a component',
-    exercises:14
-  }
-]
-}
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-]
-const len = anecdotes.length;
-const points = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0}
-
-
 const Header = (c)=>{ 
   
   return (    
@@ -154,6 +123,36 @@ const Vote = (props) => {
 
 
 const App = ()=>{
+  
+  const course = {
+  name:'Half Stack application development',
+  parts: [
+  {
+    name:'Fundamentals of React',
+    exercises: 10
+  },
+  {
+    name:'Using props to pass data',
+    exercises:7
+  },
+  {
+    name:'State of a component',
+    exercises:14
+  }
+  ]
+  }
+
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
+  ]
+  const len = anecdotes.length;
+  const points = {0:0, 1:0,2:0,3:0,4:0,5:0,6:0}
   
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
