fca205bf84de5d0e67af877cb11a2de30d66c08b
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-06-12 21:03:25 +0530

    ex4.2 blog backend refactored

---
+++ Part-4/blog-backend/controllers/blogs.js
@@ -0,0 +1,33 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+  
+  blogsRouter.delete('/:id', (req, resp) => {
+      Blog.findByIdAndRemove(req.params.id)
+          .then(result => {
+            resp.status(204).end()
+        });
+
+})     
+
+
+module.exports =  blogsRouter;
--- Part-4/blog-backend/index.js
+++ Part-4/blog-backend/index.js
@@ -1,61 +1,10 @@
-require('dotenv').config()
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id =returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-const Blog = mongoose.model('Blog', blogSchema)
+const server = http.createServer(app)
 
-process.env.MONGODB_URI
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.delete('/api/blogs/:id', (req, resp) => {
-    Blog.findByIdAndRemove(req.params.id)
-        .then(result => {
-            resp.status(204).end()
-        });
-
-})
-
-const PORT = process.env.PORT
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
