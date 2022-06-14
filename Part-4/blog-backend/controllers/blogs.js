a0a7bff580ce3374e6be4f1ed8666e0641cc8b19
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-06-15 02:41:23 +0530

    ex4.8 complete routhndlr refactor: to async/await

--- Part-4/blog-backend/controllers/blogs.js
+++ Part-4/blog-backend/controllers/blogs.js
@@ -3,29 +3,29 @@ const Blog = require('../models/blog')
 
 
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.status(200).json(blogs)
-      })
-  })
+blogsRouter.get('/', async(request, response) => {
+     
+  let blogs = await Blog.find({})
+
+  response.status(200).json(blogs)
+
+})
   
-  blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
   
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+  let result= blog.save()
+
+  response.status(201).json(result)
+
+})
   
-  blogsRouter.delete('/:id', (req, resp) => {
-      Blog.findByIdAndRemove(req.params.id)
-          .then(result => {
-            resp.status(204).end()
-        });
+blogsRouter.delete('/:id', async (req, resp) => {
+      
+  await Blog.findByIdAndRemove(req.params.id)
+
+  resp.status(204).end()
+
 
 })     
 
